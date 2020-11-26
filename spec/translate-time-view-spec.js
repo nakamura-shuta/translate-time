770557ed2e4f4ba52cbf921f8880645b2df28baa
nakamura shuta <nakamura.shuta@classmethod.jp>
2020-11-26 15:35:32 +0900

    first commit

---
+++ spec/translate-time-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TranslateTimeView from '../lib/translate-time-view';
+
+describe('TranslateTimeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
